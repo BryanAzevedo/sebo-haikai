9f3a5b9dbab507b49bea783414ed0ed045e608d6
Bryan Azevedo <bryan42damasceno@gmail.com>
2020-12-15 14:27:25 -0300

    ADD TERMINADO

--- src/Pages/Resumo/Resumo.js
+++ src/Pages/Resumo/Resumo.js
@@ -1,56 +1,54 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../Components/Footer/Footer";
-import { Button, Comment, Form, Header } from 'semantic-ui-react'
-import Comments from "../Components/Comments/Comments"
-import CustomizedRatings from '../Components/Rating/Rating'
+import { Button, Comment, Form, Header } from "semantic-ui-react";
+import Comments from "../Components/Comments/Comments";
+import CustomizedRatings from "../Components/Rating/Rating";
 import "./Resumo.css";
+import api from "../../services/api";
 
 require("typeface-quicksand");
 
-function Resumo( props ){
-    const [data, setData] = useState('');
-    useEffect(() => {
-        setData(props.location.state.livro)
-      }, []);
+function Resumo(props) {
+  const [books, setBooks] = useState([]);
 
-    return (
-        <>
-            <div className="resumo">
-                    <div className="imagem-livro">
-                        <div class="hover01">
-                            <img className="img-format" src={data.pathName}/>
-                        </div>
-                    </div>
-                    
-                    <div className="dados">
-                        <div className="titulo-livro">
-                            {data.nome}
-                        </div>
+  const obterLivros = async () => {
+    const resultado = await api.get("books");
+    console.log(resultado);
+    setBooks(resultado.data);
+  };
 
-                        <div className="autor-livro">
-                            {data.autor}
-                        </div>
+  useEffect(() => {
+    obterLivros();
+  }, []);
 
-                        <div className="descricao-livro">
-                            {data.descricao}
-                        </div>
-                        <div>
-                            <h5 className = "Rating">Já leu este livro? Dê uma avaliação!</h5>
-                            <CustomizedRatings />
-                        </div>
-
-                        <div className = "Comments">
-                            <Comments 
-                            comment={data.comment} 
-                            name={data.name} 
-                            time={data.time} 
-                            icon={data.icon}/>
-                        </div>
-                    </div>
+  return (
+    <>
+      {books.map((listItem) => {
+        return (
+          <div className="resumo">
+            <div className="imagem-livro">
+              <div class="hover01">
+                <img className="img-format" src={listItem.foto} />
+              </div>
             </div>
-            <Footer/>
-        </>
-    );
+
+            <div className="dados">
+              <div className="titulo-livro">{listItem.name}</div>
+
+              <div className="autor-livro">{listItem.autor}</div>
+
+              <div className="descricao-livro">{listItem.resumo}</div>
+              <div>
+                <h5 className="Rating">Já leu este livro? Dê uma avaliação!</h5>
+                <CustomizedRatings />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <Footer />)
+    </>
+  );
 }
 
 export default Resumo;
